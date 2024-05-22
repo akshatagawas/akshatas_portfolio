58ae4b8cd9f781a5460e497b0e9342112c1a1868
agawas <agawas@iu.edu>
2024-05-22 15:45:54 -0400

    changed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ import Contact from './pages/contact/Contact'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/akshatas_portfolio'>
       <Navbar/>
       <Themes/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' index element={<Home/>}/>
         <Route path='about' element={<About/>}/>
         <Route path='projects' element={<Projects/>}/>
         <Route path='contact' element={<Contact/>}/>
