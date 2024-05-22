71cebff8e869d748018b54960d21a919515c4d23
agawas <agawas@iu.edu>
2024-05-22 15:35:52 -0400

    changed vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Navbar/>
       <Themes/>
       <Routes>
-        <Route path='/' index element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='about' element={<About/>}/>
         <Route path='projects' element={<Projects/>}/>
         <Route path='contact' element={<Contact/>}/>
